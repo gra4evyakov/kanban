45f0dc3316986e3cd875e8da473edca5349a9192
gra4evyakov <gra4evyakov@yandex.ru>
2023-08-14 17:39:30 +0300

    add sort

--- src/stores/kanban.js
+++ src/stores/kanban.js
@@ -1,4 +1,4 @@
-import { ref, watchEffect } from 'vue' // добавить computed
+import { ref, computed, watchEffect } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useKanbanStore = defineStore('kanban', () => {
@@ -7,177 +7,208 @@ export const useKanbanStore = defineStore('kanban', () => {
       {
         id: 1,
         name: 'перекрасить кнопку',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '09:09',
+        date: '11.08.2023'
       },
       {
         id: 2,
         name: 'удалить что-то',
         description: 'пофигу что',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '10:10',
+        date: '19.08.2023'
       },
       {
         id: 3,
         name: 'уволиться',
         time: '11:11',
-        date: '12.08.2023'
+        date: '10.08.2023'
       },
       {
         id: 4,
         name: 'тест api',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '12:12',
+        date: '11.08.2023'
       },
       {
         id: 5,
         name: 'перекур',
-        time: '11:11',
+        time: '13:13',
         date: '12.08.2023'
       },
       {
         id: 6,
         name: 'сходить в бар',
         description: 'помни о пиве',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '14:14',
+        date: '13.08.2023'
       }
     ]),
     inProgress: ref([
       {
         id: 7,
         name: 'стереть базу данных',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '15:15',
+        date: '12.05.2023'
       },
       {
         id: 8,
         name: 'обновить пк',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '16:16',
+        date: '31.01.2023'
       },
       {
         id: 9,
         name: 'попросить повышение',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '17:17',
+        date: '27.08.2023'
       },
       {
         id: 10,
         name: 'покормить кошку',
         description: 'очень много ест',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '18:18',
+        date: '12.09.2023'
       },
       {
         id: 11,
         name: 'отдых',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '19:19',
+        date: '11.11.2023'
       },
       {
         id: 12,
         name: 'удалить сервер',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '20:20',
+        date: '05.10.2023'
       }
     ]),
     inQA: ref([
       {
         id: 13,
         name: 'сделать тестовое',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '08:08',
+        date: '10.10.2023'
       },
       {
         id: 14,
         name: 'переехать',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '22:22',
+        date: '16.03.2023'
       },
       {
         id: 15,
         name: 'целоваться',
         description: 'со всеми подряд',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '09:09',
+        date: '07.02.2023'
       },
       {
         id: 16,
         name: 'обниматься',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '21:21',
+        date: '12.04.2023'
       },
       {
         id: 17,
         name: 'зарядка',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '10:10',
+        date: '17.06.2023'
       },
       {
         id: 18,
         name: 'отсылать мемасы',
-        time: '11:11',
+        time: '20:20',
         date: '12.08.2023'
       },
       {
         id: 19,
         name: 'игнорить начальника',
         time: '11:11',
-        date: '12.08.2023'
+        date: '03.08.2023'
       },
       {
         id: 20,
         name: 'погулять с собакой',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '19:19',
+        date: '19.03.2023'
       },
       {
         id: 21,
         name: 'смотреть ютуб',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '12:12',
+        date: '12.02.2023'
       },
       {
         id: 22,
         name: 'встретиться с друзьями',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '18:18',
+        date: '07.07.2023'
       },
       {
         id: 23,
         name: 'помыться',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '13:13',
+        date: '01.01.2023'
       },
       {
         id: 24,
         name: 'почистить зубки',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '17:17',
+        date: '05.05.2023'
       },
       {
         id: 25,
         name: 'улыбаться',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '14:14',
+        date: '20.04.2023'
       }
     ]),
     done: ref([
       {
         id: 26,
         name: 'работать',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '16:16',
+        date: '17.02.2023'
       },
       {
         id: 27,
         name: 'не работать',
-        time: '11:11',
-        date: '12.08.2023'
+        time: '15:15',
+        date: '11.01.2023'
       }
     ])
   }
+  const sortList = [
+    {
+      label: 'по имени',
+      value: 'name'
+    },
+    {
+      label: 'по дате',
+      value: 'date'
+    },
+    {
+      label: 'по времени',
+      value: 'time'
+    }
+  ]
+  const selectedSortOption = ref('date')
+
+  const sortedSections = computed(() => {
+    const sorted = { ...sections }
+    for (const sectionKey of Object.keys(sorted)) {
+      sorted[sectionKey].value = sorted[sectionKey].value.sort((a, b) => {
+        if (selectedSortOption.value === 'name') {
+          return a.name.localeCompare(b.name)
+        } else if (selectedSortOption.value === 'date') {
+          return a.date.localeCompare(b.date)
+        } else if (selectedSortOption.value === 'time') {
+          return a.time.localeCompare(b.time)
+        }
+      })
+    }
+    return sorted
+  })
 
   for (const sectionKey of Object.keys(sections)) {
     const storedSection = localStorage.getItem(`kanban_${sectionKey}`)
@@ -191,24 +222,6 @@ export const useKanbanStore = defineStore('kanban', () => {
     }
   })
 
-  // РЕАЛИЗАЦИЯ ПОИСКА ЧЕРЕЗ COMPUTED
-
-  // const searchQuery = ref('');
-
-  // const filteredTasks = computed(() => {
-  //     const query = searchQuery.value.toLowerCase();
-  //     const results = {};
-
-  //     for (const sectionKey of Object.keys(sections)) {
-  //       const tasksInSection = sections[sectionKey].value;
-  //       const filteredTasksInSection = tasksInSection.filter(task =>
-  //         task.name.toLowerCase().includes(query) || (task.description && task.description.toLowerCase().includes(query))
-  //       );
-  //       results[sectionKey] = filteredTasksInSection;
-  //     }
-  //     return results;
-  // });
-
   const addTask = (task) => {
     sections.backlog.value.push(task)
     localStorage.setItem('kanban_backlog', JSON.stringify(sections.backlog.value))
@@ -221,11 +234,11 @@ export const useKanbanStore = defineStore('kanban', () => {
     }
   }
 
-  // filteredTasks и seachQuery для поиска
   return {
     sections,
-    // filteredTasks,
-    // searchQuery,
+    sortList,
+    selectedSortOption,
+    sortedSections,
     addTask,
     removeTaskById
   }
